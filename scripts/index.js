74ef610976568ac007de1d64054c290865381c88
Bulat Zinatullin <Zinatullinbn@gmail.com>
2024-03-23 20:24:56 +0300

    mestoxx

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsContainer = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function createCard(card, deleteCard){
+    const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+    cardItem.querySelector('.card__image').src = card.link;
+    cardItem.querySelector('.card__image').alt = card.name;
+    cardItem.querySelector('.card__title').textContent = card.name;
+    const deleteCardButton = cardItem.querySelector('.card__delete-button');
+    deleteCardButton.addEventListener('click', function(){
+        deleteCard(cardItem)});
+    return cardItem;
+}
 
-// @todo: Функция создания карточки
+function deleteCard(cardItem){
+    return cardItem.remove();
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function(card){
+    const cardElement = createCard(card, deleteCard);
+    cardsContainer.append(cardElement);
+});
